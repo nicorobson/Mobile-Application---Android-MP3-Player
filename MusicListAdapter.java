f726a8d6b0e4eaa4e3a4d96b8427da6c2e27bb58
Nico Robson <97481260+nicorobson@users.noreply.github.com>
2023-01-14 16:16:24 -0500

    Add files via upload

---
+++ MusicListAdapter.java
@@ -0,0 +1,53 @@
+package com.example.filefunctionality;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.ViewHolder>{
+
+    ArrayList<AudioModel> songsList;
+    Context context;
+
+    public MusicListAdapter(ArrayList<AudioModel> songsList, Context context) {
+        this.songsList = songsList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+        return new MusicListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder( MusicListAdapter.ViewHolder holder, int position) {
+        AudioModel songData = songsList.get(position);
+        holder.titleTextView.setText(songData.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleTextView;
+        ImageView iconImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.music_title_text);
+            iconImageView = itemView.findViewById(R.id.icon_view);
+        }
+    }
+}
